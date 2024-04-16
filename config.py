5baca042caddea5ec06186dda3a56e86d9b94e9e
chinai807 <167169288+chinai807@users.noreply.github.com>
2024-04-16 14:46:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.ctyioa0868gh.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "tanchinai-employee"
+customregion = "us-east-1"
+
